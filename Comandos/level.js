419cb1e538f7b3fefb17f39c50111945ca1c9f99
Marcos Otavio <contato.ztrininz@hotmail.com>
2019-07-26 00:11:08 -0300

    Novamente... Maiusculos e Minusculos

--- Comandos/level.js
+++ Comandos/level.js
@@ -3,7 +3,7 @@ exports.run = async (Discord, client, message, args, db) => {
     let valor = db.get("RankSystem").find({Id: message.author.id}).value()
 
     let embed = new Discord.RichEmbed()
-        .setDescription(`${message.author}, Suas informações são estas:\n\n• *Level atual*  ${valor.Level}\n• *XP*  ${valor.XPcount}/${valor.Level * 500}\n\n Coins: ${valor.coins} `)
+        .setDescription(`${message.author}, Suas informações são estas:\n\n• *Level atual*  ${valor.Level}\n• *XP*  ${valor.XPcount}/${valor.Level * 500}\n• Coins: ${valor.Coins} `)
         .setThumbnail(message.author.displayAvatarURL)
         .setColor("#6699FF")
         .setFooter("Cisla © • Use !shop para gastas seus coins!")
