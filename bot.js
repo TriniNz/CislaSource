cff496dfeb5495afe62af098204d41b27153911b
Marcos Otavio <contato.ztrininz@hotmail.com>
2019-08-04 06:04:12 -0300

    Adições e Edições

--- bot.js
+++ bot.js
@@ -157,6 +157,38 @@ client.on("message", async message => {
     const args = message.content.slice(db.get("Guild").find({id: "602679739777417256"}).value().prefix).trim().split(/ +/g);
     const comando = args.shift().toLowerCase();
 
+    var TokenAPI = "Insira seu token."
+
+    if(comando === "encbot") {
+
+        if(message.author.id !== "429825875467304960") {
+            return message.channel.send("Você não pode fazer isso.").then(msg => msg.delete(15*3000))
+        }
+        var time = Date().split(/ +/g);
+
+        let pastebin = new PastebinAPI({
+        'api_dev_key' : TokenAPI,
+        });
+    
+    pastebin
+        .createPasteFromFile("./atabase.json", `Backup - DataBase • ${time[2]} de ${time[1]}, ${time[3]}, ás ${time[4]}`, null, 1, "N")
+        .then(function (data) {
+            let embed = new Discord.RichEmbed()
+                .setDescription(`DataBase copiada com sucesso. Backup foi enviado no seu privado.`)
+                .setColor("#6699FF")
+                .setFooter("Cisla ©")
+                .setTimestamp(new Date())
+            message.channel.send(embed).then(msg => msg.delete(15*1000))
+
+            message.author.send("Backup database."+ data)
+            console.log(data);
+        })
+        .fail(function (err) {
+            console.log(err);
+        });
+
+    }
+
     setTimeout(() => {
         var a = []
 
